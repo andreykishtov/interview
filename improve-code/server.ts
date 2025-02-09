dcc4587e117ae4eef6e7df25f7b7a62288dbea3d
Andrey Kishtov <andreyk@jfrog.com>
2025-02-09 10:46:45 +0200

    added server.ts

---
+++ improve-code/server.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "GET request successful" });
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.json({ message: "POST request successful" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
